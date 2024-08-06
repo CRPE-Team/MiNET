af6cdfbe9122d22de4b1198ffbb4a3cd9ab37070
Алексей Лашов <lashajahsnal@gmail.com>
2024-08-06 22:49:50 +0300

    block states tracer

--- src/MiNET/MiNET/Plugins/Commands/DebugCommands.cs
+++ src/MiNET/MiNET/Plugins/Commands/DebugCommands.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using MiNET.Blocks;
 using MiNET.Plugins.Attributes;
 using MiNET.Utils.Vectors;
@@ -9,6 +10,7 @@ namespace MiNET.Plugins.Commands
 	public class DebugCommands
 	{
 		private readonly Dictionary<string, BlockCoordinates> _blocksLocationMap = new Dictionary<string, BlockCoordinates>();
+		private readonly List<Player> _blockStatesTracerSubscribers = new List<Player>();
 
 		[Command(Name = "toblock")]
 		public void TpAnvilDebugWorldBlock(Player player, BlockTypeEnum blockType)
@@ -51,6 +53,62 @@ namespace MiNET.Plugins.Commands
 
 			player.Teleport(existingBlockLocation + new PlayerLocation(0.5f, 1, 0.5f));
 			player.SendMessage($"Found block [{requieredBlockName}] at [{existingBlockLocation}]");
+		}
+
+		[Command(Name = "statestracer")]
+		public void BlockStatesTracer(Player player, bool enable)
+		{
+			lock (_blockStatesTracerSubscribers)
+			{
+				if (enable && !_blockStatesTracerSubscribers.Contains(player))
+				{
+					player.Ticking += TraceBlock;
+					_blockStatesTracerSubscribers.Add(player);
+				}
+				else
+				{
+					player.Ticking -= TraceBlock;
+					_blockStatesTracerSubscribers.Remove(player);
+				}
+			}
+		}
+
+		private void TraceBlock(object soruce, PlayerEventArgs args)
+		{
+			var ray = new Ray(args.Player.KnownPosition + new Vector3(0, 1.62f, 0), args.Player.KnownPosition.GetHeadDirection());
+			var point = ray.Position;
+			var shift = ray.Direction / Vector3.Abs(ray.Direction);
+
+			for (var i = 0; i < 20; i++)
+			{
+				if (Check(point)
+					|| Check(point + Vector3.UnitX * shift)
+					|| Check(point + Vector3.UnitY * shift)
+					|| Check(point + Vector3.UnitZ * shift)
+					|| Check(point + (Vector3.UnitX + Vector3.UnitY) * shift)
+					|| Check(point + (Vector3.UnitX + Vector3.UnitZ) * shift)
+					|| Check(point + (Vector3.UnitY + Vector3.UnitZ) * shift))
+				{
+					break;
+				}
+
+				bool Check(BlockCoordinates coords)
+				{
+					var block = args.Level.GetBlock(coords);
+					if (block is Air)
+						return false;
+
+					var ds = ray.Intersects(block.GetBoundingBox());
+					if (!ds.HasValue || ds <= 0)
+						return false;
+
+					args.Player.SendMessage($"Id: {block.Id}\n{string.Join("\n", block.States.Select(s => $"{s.Name}: {s.GetValue()}"))}", MessageType.Tip);
+
+					return true;
+				}
+
+				point += ray.Direction;
+			}
 		}
 	}
 }
